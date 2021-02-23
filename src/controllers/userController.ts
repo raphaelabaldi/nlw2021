d3bd62d8e5ec1a576339301665a9a43ed4cd9173
raphaela <raphaelabaldi12345@gmail.com>
2021-02-23 16:22:32 -0300

    add: user controller and entity

---
+++ src/controllers/userController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/user'
+
+class UserController {
+    async create(request: Request, response: Response) {
+        const { name, email } = request.body;
+        const userRepository = getRepository(User);
+        const userAlreadyExists = await userRepository.findOne({ email })
+
+        if (userAlreadyExists) {
+            return response.status(400).json({
+                error: "This user already exists!"
+            })
+        }
+
+        const user = userRepository.create({
+            name, email
+        });
+
+        await userRepository.save(user);
+
+        return response.json(user);
+    }
+}
+
+export { UserController };
