2e8070e23753f802ac05f9d5efea8262ee1cf21d
raphaela <raphaelabaldi12345@gmail.com>
2021-02-23 16:22:53 -0300

    change: server routes to new file routes.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
+import 'reflect-metadata';
+import './database';
 import express, { request } from 'express';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World!' });
-});
-app.post('/', (request, response) => {
-    //receber os dados
-    return response.json({ message: 'Data saved successfully!' });
-});
+app.use(express.json());
+app.use(router)
 
 app.listen(3333, () => console.log('Server is up and running!'));
