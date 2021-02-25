826cdcbffd02738fbfadfec2618b5cd2a69f6fa0
raphaela <raphaelabaldi12345@gmail.com>
2021-02-25 19:18:06 -0300

    refactor: app is now separate from server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,4 @@
-import 'reflect-metadata';
-import './database';
-import express, { request } from 'express';
-import { router } from './routes';
-
-const app = express();
-
-app.use(express.json());
-app.use(router)
+import { app } from "./app";
 
 app.listen(3333, () => console.log('Server is up and running!'));
+
