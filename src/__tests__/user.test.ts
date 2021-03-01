09ad5e81c8efaa5e8ee82dcfbb96134fb1ba8fd9
raphaela <raphaelabaldi12345@gmail.com>
2021-03-01 20:53:48 -0300

    feat: user test

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -1,10 +1,22 @@
 import request from "supertest";
 import { app } from "../app";
 
+import createConnection from '../database'
+
+
+createConnection();
 describe("users", () => {
-    request(app).post("/users")
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    })
+})
+
+it('Should be able to create a new user', async () => {
+    const response = await request(app).post("/users")
         .send({
             email: "user@example.com.br",
             name: "user"
-        })
+        });
+    expect(response.status).toBe(201)
 })
